2332d3a9c038483ae618a15c12604fcfba12497e
hp <hp@hp-PC>
2018-08-30 13:20:52 +0530

    com.myproject first commit

---
+++ com.myproject.pageobjectmodel/src/test/java/com/myproject/pageobjectmodel/POMDemo.java
@@ -0,0 +1,10 @@
+package com.myproject.pageobjectmodel;
+import org.testng.annotations.Test;
+public class POMDemo {
+
+@Test
+public void PrintSel()
+{
+System.out.println("This is my first program");
+}
+}
